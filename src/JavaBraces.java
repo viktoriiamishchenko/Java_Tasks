9d047a12499bbed64c280923d96bffe7f4da8d5b
Viktoriia M <viktoriiamish@gmail.com>
2021-11-13 22:01:22 -0500

    first commit

---
+++ src/JavaBraces.java
@@ -0,0 +1,32 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class JavaBraces {
+
+    public static boolean checkBrackets (String str) {
+        //FIFO
+        //LIFO
+        Deque<Character> deque = new LinkedList<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                deque.add(ch);
+            } else {
+                if(deque.size() == 0 || deque.pollLast() != '(') {
+                    return false;
+                }
+            }
+        }
+
+        return deque.size() == 0;
+    }
+
+    public static void main (String [] args) {
+        //((())()())
+
+        boolean result = checkBrackets("((())()())");
+        System.out.println(result);
+
+
+    }
+}
---
+++ src/JavaManyBraces.java
@@ -0,0 +1,43 @@
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class JavaManyBraces {
+
+
+        private static Map<Character, Character> map;
+        static {
+            map = new HashMap<>();
+            map.put(')', '(');
+            map.put(']', '[');
+            map.put('}', '{');
+        }
+        public static boolean checkBrackets (String str){
+
+            Deque<Character> deque = new LinkedList<>();
+            for (int i = 0; i < str.length(); i++) {
+                Character closingBracket = str.charAt(i);
+                Character openingBracket = map.get(closingBracket);
+                if (openingBracket == null) {
+                    deque.add(closingBracket);
+                } else {
+                    if (deque.size() == 0 || deque.pollLast() != openingBracket) {
+                        return false;
+                    }
+                }
+            }
+
+            return deque.size() == 0;
+        }
+
+
+    public static void main (String [] args) {
+        //((())()())
+
+        boolean result = checkBrackets("{(())[]()()}");
+        System.out.println(result);
+
+
+    }
+}
